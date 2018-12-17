bef3a5b69c74011f0e47f06930c040acc74ec2b7
RayKilburg <rkilburg42@gmail.com>
2018-12-17 13:57:17 -0500

    social media links added to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      &copy; Gatsby Starter - Dimension. Design:{' '}
+      <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+    </p>
+    <ul className="icons">
+      <li>
+        <a href="#" className="icon fa-twitter">
+          <span className="label">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="icon fa-facebook">
+          <span className="label">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="icon fa-instagram">
+          <span className="label">Instagram</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" className="icon fa-github">
+          <span className="label">GitHub</span>
+        </a>
+      </li>
+    </ul>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: PropTypes.bool
+  timeout: PropTypes.bool,
 }
 
 export default Footer
