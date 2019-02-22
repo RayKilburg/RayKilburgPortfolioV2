dec8995a0c07cf90b7da56c1be8ee7eaefc193ce
RayKilburg <rkilburg42@gmail.com>
2019-02-21 19:47:22 -0500

    added netlify data form to contact page

--- src/components/Main.js
+++ src/components/Main.js
@@ -229,7 +229,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form method="post" data-netlify="true" action="#">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
